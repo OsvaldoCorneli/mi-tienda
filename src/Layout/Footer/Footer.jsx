dbd47ddd65f820c2635cb88a19cafe9cb00108be
OsvaldoCorneli <overcorneli@gmail.com>
2026-05-28 23:53:57 -0300

    Realice algunas actualizacion en el css, modifique footer, no funciona menu hamburguesa

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -7,22 +7,25 @@ function Footer() {
 
         <footer className={footerCss.container}>
 
-            <div className={footerCss.container2}>
-                <TeamListContainer/>
-                <p>© 2026 Proyecto académico desarrollado con React.js para el curso de React de Talento Tech.</p>
-                <p>Las imágenes, marcas y elementos visuales utilizados pertenecen a sus respectivos autores y 
-                    fueron utilizados únicamente con fines educativos y sin intención comercial.</p>
+            <div className={footerCss.container1}>
+
+                <div className={footerCss.container2}>
+                    <TeamListContainer />
+                    <img className={footerCss.Logoimg} src={imageFooter} alt="" />
+                </div>
+
+                <div className={footerCss.container3}>
+
+                    <p>© 2026 Proyecto académico desarrollado con React.js para el curso de React de Talento Tech.</p>
+                    <p>Las imágenes, marcas y elementos visuales utilizados pertenecen a sus respectivos autores y
+                        fueron utilizados únicamente con fines educativos y sin intención comercial.</p>
+                </div>
             </div>
 
-            <div className={footerCss.container3}>
-                <img src={imageFooter} alt="" />
 
-             </div>
+        </footer >
 
-
-    </footer >
-
-)
+    )
 }
 
 export default Footer;
