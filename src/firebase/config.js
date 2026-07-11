71c10bf71cc3cecffbf87dbc02dc2f5328646d1c
OsvaldoCorneli <overcorneli@gmail.com>
2026-07-11 11:27:47 -0300

    Cargue la Db y ahora falta traer los productos a la pagina principal

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDMyDsM6UTKVFaeFSZEmBpTm6hLr84itoc",
-  authDomain: "mitienda-ecommerce-reactjs.firebaseapp.com",
-  projectId: "mitienda-ecommerce-reactjs",
-  storageBucket: "mitienda-ecommerce-reactjs.firebasestorage.app",
-  messagingSenderId: "1008109299233",
-  appId: "1:1008109299233:web:3e26a1ee8b3a11a3c7e560"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
