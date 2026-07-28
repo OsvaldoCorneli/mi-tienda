2cea7701361657efcd04b7f098e7127223f3b026
OsvaldoCorneli <overcorneli@gmail.com>
2026-07-28 16:37:00 -0300

    terminando paginado, revisar la funcion getproductsOnsale

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -9,53 +9,41 @@ import Pagination from "../Pagination/Pagination.jsx";
 function ItemListContainer({ mensaje, onSales }) {
   const { products, loading, getProductoOnSale } = useProducts();
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(12);
-  const [totalProducts, SetTotalProducts] = useState(0)
+  const postsPerPage = 12;
   const lastPostIndex = currentPage * postsPerPage;
   const firstPostIndex = lastPostIndex - postsPerPage;
-  const currentPosts = products.slice(firstPostIndex, lastPostIndex);
-  const currentPostsOnsale = getProductoOnSale().slice(firstPostIndex, lastPostIndex)
+  
+  const productsToShow = onSales 
+  ?  getProductoOnSale()
+  :  products
 
+  console.log('toshow', productsToShow)
+  
+  const currentPosts = productsToShow.slice(firstPostIndex, lastPostIndex);
+  
+  console.log("current",currentPosts)
 
-  return (
+   return (
     <>
       {loading ? (
         <SkeletonContainer />
       ) : (
         <div>
-            {
-                !onSales 
-                ?  <ItemList
+            <ItemList
             products={currentPosts}
             onSales={onSales}
             mensaje={mensaje}
             precioFinal={calcularOferta}
             formatearPrecio={formatearPrecio}
           />
-                :  <ItemList
-            products={currentPostsOnsale}
-            onSales={onSales}
-            mensaje={mensaje}
-            precioFinal={calcularOferta}
-            formatearPrecio={formatearPrecio}
-          />
-
-            }
+              
          
-          {
-            onSales 
-            ? <Pagination
-          totalPosts={getProductoOnSale().length}
+     <Pagination
+          totalPosts={productsToShow.length}
           postsPerPage={postsPerPage}
           setCurrentPage={setCurrentPage}
           currentPage={currentPage} />
-          : <Pagination
-          totalPosts={totalProducts}
-          postsPerPage={postsPerPage}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage} />
-
-          }
+          
           
         </div>
       )}
